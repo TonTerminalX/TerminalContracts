f85d13059751c0fee39cf3b97630a6d6db249f6c
Oleg Syniakevych <olegsyniakevych@gmail.com>
2024-11-23 00:54:28 +0200

    Jetton Swap

--- scripts/interactJettonReceive.ts
+++ scripts/interactJettonReceive.ts
@@ -5,7 +5,7 @@ import { NetworkProvider, sleep } from '@ton/blueprint';
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse(`UQA80gmudfe3WfSok_5JXWEHNYHx9rvh7sdOtPrSYNTmpZE4`);
+    const address = Address.parse(`EQCtZLCoXaqWaouizgXHs3DGY-Mk3QXOdjmyEZRLBfSsG6SO`);
 
     if (!(await provider.isContractDeployed(address))) {
         ui.write(`Error: Contract at address ${address} is not deployed!`);
@@ -18,30 +18,4 @@ export async function run(provider: NetworkProvider, args: string[]) {
     const jettonWalletAddress = await simpleCounter.getMyJettonWalletAddress();
     console.log(`Jetton amount: ${jettonAmount}`);
     console.log(`Jetton wallet address: ${jettonWalletAddress}`);
-
-    // await simpleCounter.send(
-    //     provider.sender(),
-    //     {
-    //         value: toNano('0.05'),
-    //     },
-    //     {
-    //         $$type: 'Add',
-    //         queryId: 0n,
-    //         amount: 1n,
-    //     }
-    // );
-
-    ui.write('Waiting for counter to increase...');
-
-    // let counterAfter = await simpleCounter.getMyJettonAmount();
-    // let attempt = 1;
-    // while (counterAfter === counterBefore) {
-    //     ui.setActionPrompt(`Attempt ${attempt}`);
-    //     await sleep(2000);
-    //     counterAfter = await simpleCounter.getMyJettonAmount();
-    //     attempt++;
-    // }
-
-    // ui.clearActionPrompt();
-    // ui.write('Counter increased successfully!');
 }
