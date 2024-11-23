6e3c338f6755a62fe74ef074c7b9564089b1c7a3
Oleg Syniakevych <olegsyniakevych@gmail.com>
2024-11-23 04:04:44 +0200

    Successful swap POVEL to ton

--- scripts/interactJettonReceive.ts
+++ scripts/interactJettonReceive.ts
@@ -5,7 +5,7 @@ import { NetworkProvider, sleep } from '@ton/blueprint';
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse(`EQCtZLCoXaqWaouizgXHs3DGY-Mk3QXOdjmyEZRLBfSsG6SO`);
+    const address = Address.parse(`EQA80gmudfe3WfSok_5JXWEHNYHx9rvh7sdOtPrSYNTmpcz9`);
 
     if (!(await provider.isContractDeployed(address))) {
         ui.write(`Error: Contract at address ${address} is not deployed!`);
